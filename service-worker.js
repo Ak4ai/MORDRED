00a0a771e2eedcfa902ba8d42381b590b5cce796
Ak4ai <129908980+Ak4ai@users.noreply.github.com>
2025-04-23 12:20:28 -0300

    5

--- service-worker.js
+++ service-worker.js
@@ -2,16 +2,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('my-cache').then(function(cache) {
         return cache.addAll([
-          '/',
-          '/Singularidade.html',
-          '/style.css', // Adicione todos os arquivos necessários para o offline
-          '/script.js',
-          '/inventario.js',
-          '/map.js',
-          '/youtube.js',
-          '/firebaseconfig.js',
-          '/google.js',
-          '/chat.js',
+          '/'
         ]);
       })
     );
